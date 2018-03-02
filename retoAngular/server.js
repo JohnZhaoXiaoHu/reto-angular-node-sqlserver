b0640144b3ecc91b065399a5bd941189369f55b1
leonidas999 <leandro19921@hotmail.com>
2018-03-02 14:19:40 -0500

    cambios server

--- retoAngular/server.js
+++ retoAngular/server.js
@@ -36,30 +36,30 @@ var dbConfig = {
    };
 
 //Function to connect to database and execute query
-var  executeQuery = function(respond, query){
+var  executeQuery = function(respond,query){
      sql.connect(dbConfig, function (err) {
          if (err) {
                      console.log("Error while connecting database :- " + err);
                      res.send(err);
-                  }
-                  else {
-                         // create Request object
-                         var request = new sql.Request();
-                         // query to the database
-                         request.query(query, function (err, res) {
-                           if (err) {
-                                      console.log("Error while querying database :- " + err);
+                  }else{
+                    var request = new sql.Request();
+                    // query to the database
+                    request.query(query, function (err, res) {
+                      if (err) {
+                                 console.log("Error while querying database :- " + err);
+                                 respond.send(err);
+                                }
+                                else {
+                                  console.log(res);
+                                  respond.send(res);
+                                 }
+                          });
 
-                                      respond.json("no se puedo crear el usuario");
-                                     }
-                                     else {
-                                       console.log(res);
-                                       respond.send(res);
-                                      }
-                               });
-                       }
+                  }
       });
 }
+
+
 
 
 
@@ -73,26 +73,15 @@ app.get('/cliente', function (req, res) {
 //POST API
  app.post("/cliente/create", function(req , res){
                 console.log("peticion post!!");
+
                 console.log(req.body);
+               
+                  
                 var query = "INSERT INTO [Cliente] (Nombre,Apellido,Cedula,Usuario,Contrasena,fecha_Nacimiento) VALUES ('" + req.body.Nombre + "','" + req.body.Apellido + "','" + req.body.Cedula + "','" + req.body.Usuario  + "','" + req.body.Contrasena  + "','" + req.body.fecha_Nacimiento + "')";
                 executeQuery (res, query);
+                
 });
 
-
-
-
-//PUT API
- app.put("/cliente/update/:id", function(req , res){
-                var query = "UPDATE [cliente] SET Nombre= '" + req.body.nombre  +  "', Apellido=  '" + req.body.apellido + "'  WHERE id_Usuario= '" + req.params.id;
-                executeQuery (res, query);
-});
-
-// DELETE API
- app.post("/cliente/delete/:id", function(req , res){
-                console.log("deleting client");
-                var query = "DELETE FROM [cliente] WHERE id_Usuario='"  + req.params.id;
-                executeQuery (res, query);
-});
 
 
 
